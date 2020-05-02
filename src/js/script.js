f6c2fe0aae5bcedc52af65a927c573283bba889d
DocOrNotADoc <leesunsinn@protonmail.com>
2020-05-03 01:13:02 +0300

    jqueryvalidate, section managers

---
+++ src/js/script.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+
+    function validateForms(form){
+        $(form).validate({
+            rules: {
+                name: {                
+                    required: true,
+                    minlength: 2
+                },
+                phone: "required",
+                email: {
+                    required: true,
+                    email: true,
+                },
+            },
+            messages: {
+                name: {
+                    required: "Пожалуйста, введите своё имя",
+                    minlength: jQuery.validator.format("Введите {0} символa!")
+                },
+                phone: "Пожалуйста, введите свой номер телефона",
+                email: {
+                    required: "Пожалуйста, введите свою почту",
+                    email: "Неправильно введён адрес почты"
+                }
+            }
+        });
+    };
+    // validateForms('#consult-form');
+    validateForms('#form-consult');
+    // validateForms('#order form');
+
+});
