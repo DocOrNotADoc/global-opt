cd094a00dda68d4fcbac1fee06df14d240445ea5
DocOrNotADoc <leesunsinn@protonmail.com>
2020-05-07 13:59:08 +0300

    map-beta footer, up

--- dist/js/script.js
+++ dist/js/script.js
@@ -28,6 +28,23 @@ $(document).ready(function(){
     };
     // validateForms('#consult-form');
     validateForms('#form-consult');
+    validateForms('#form-que');
     // validateForms('#order form');
+
+    // Smooth scroll and pageup
+
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 1600) {
+            $('.pageup').fadeIn();
+        } else {
+            $('.pageup').fadeOut();
+        }
+    });
+
+    $("a[href^='#']").click(function() {
+        const _href = $(this).attr("href");
+        $("html, body").animate({scrollTop: $(_href).offset().top+"px"});
+        return false;
+    });
 
 });
